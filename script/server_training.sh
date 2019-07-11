cf8a42f2e87c9718890fb007a4c84de873404786
linhai@axe <xielinhai@gmail.com>
2019-07-11 17:28:46 -0500

    fix

--- script/server_training.sh
+++ script/server_training.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-CUDA_VISIBLE_DEVICES=1 python script/training_rdpg_BPTT.py --model_dir /home/linhai/Work/catkin_ws/data/vpf_data/saved_network --buffer_size 1000 --max_training_step 5000000
+CUDA_VISIBLE_DEVICES=3 python script/training_rdpg_BPTT.py --model_dir /home/linhai/Work/catkin_ws/data/vpf_data/saved_network --buffer_size 1000 --max_training_step 5000000
