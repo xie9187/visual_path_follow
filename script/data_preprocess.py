f346e8ee4e5461a12ddbae18ede5749356d51601
Linhai Xie <xielinhai@gmail.com>
2019-07-19 14:51:08 +0100

    generate mean flow

--- script/data_preprocess.py
+++ script/data_preprocess.py
@@ -48,7 +48,7 @@ def generate_flow_seq(file_path_number_list, data_path, batch_size, img_size):
                                                               input_b_tf: input_b_batch
                                                               })[start:end])
                 print start, end, len(img_list)
-                assert False
+            assert False
             pred_flow_seq = np.concatenate(pred_flow_seq_list, axis=0)
             pred_flow_list = np.split(pred_flow_seq, len(pred_flow_seq), axis=0)
 
