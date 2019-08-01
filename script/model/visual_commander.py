3cc0fcf73b7e944ff63af1bf03f50566dad98d98
linhai <xielinhai@outlook.com>
2019-08-01 13:19:44 +0100

    multi gpu

--- script/model/visual_commander.py
+++ script/model/visual_commander.py
@@ -104,6 +104,8 @@ class visual_commander(object):
         outputs = []
         print splited_outputs_list
         for splited_output_list in splited_outputs_list:
+            shape = tf.get_shape(splited_output_list[0]).tolist()
+            print shape
             combiend_output = tf.concat(splited_output_list, axis=0)
             outputs.append(combiend_output)
         if len(outputs) == 1:
