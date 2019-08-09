b3ed91465228873af541c8cba9c67eff71e12041
Linhai Xie <xielinhai@gmail.com>
2019-08-09 09:15:46 +0100

    abs q error in drqn

--- script/model/drqn.py
+++ script/model/drqn.py
@@ -64,8 +64,10 @@ class Network(object):
             self.mask = tf.reshape(tf.sequence_mask(self.length, maxlen=self.max_step, dtype=tf.float32), [-1]) # b*l
             td_error = tf.square(y - selected_q) * self.mask # b*l
             if self.prioritised_replay:
-                td_error_seq = tf.reshape(td_error, [-1, self.max_step]) # b, l
-                self.q_errors = 0.1*tf.reduce_mean(td_error_seq, axis=1) + 0.9*tf.reduce_max(td_error_seq, axis=1) # b
+                td_error_abs = tf.abs(tf..reshape(td_error, [-1, self.max_step])) # b, l
+                max_q_error = tf.reduce_max(td_error_abs, axis=1) # b
+                mean_q_error = tf.reduce_sum(td_error_abs, axis=1)/tf.reduce_sum(self.mask, axis=1) # b
+                self.q_errors = 0.1*mean_q_error + 0.9* max_q_error # b
                 ISWeights = tf.reshape(tf.tile(self.ISWeights, [1, self.max_step]), [-1]) # b*l
                 td_error = td_error * ISWeights 
             loss = tf.reduce_mean(td_error)
