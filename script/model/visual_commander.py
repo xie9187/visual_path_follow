d70cf89ba5f2d6864e5d85dfe9d89feffb7603ea
linhai <xielinhai@gmail.com>
2019-08-10 19:57:59 +0100

    never attend to patted img

--- script/model/visual_commander.py
+++ script/model/visual_commander.py
@@ -326,7 +326,7 @@ class visual_commander(object):
             norm_mask = tf.sequence_mask(demo_len, maxlen=self.max_n_demo, dtype=tf.float32) # b, n
             norm_mask = tf.reshape(tf.tile(tf.expand_dims(norm_mask, axis=1), [1, self.max_step, 1]), [-1, self.max_n_demo]) # b*l, n
             # never attend to padded images
-            l2_norm += (1. - norm_mask) * 1000.
+            # l2_norm += (1. - norm_mask) * 1000.
             logits = tf.log(tf.nn.softmax(-l2_norm)) # b*l, n
             att_pos = tf.argmax(logits, axis=1) # b*l
         self.l2_norm = l2_norm
