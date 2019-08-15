dff3df46d6e71b95c2073b5e287c35c36685c9ca
linhai <xielinhai@gmail.com>
2019-08-15 21:48:07 +0100

    no activation in cnn

--- script/model/deep_metric.py
+++ script/model/deep_metric.py
@@ -73,25 +73,35 @@ class deep_metric(object):
         nega_img = tf.reshape(nega_img, [-1]+self.dim_img) #b*l,h,d,c
 
         # encoding
-        demo_vect = self.encode_image(demo_img) # b, dim_img_feat
+        demo_vect = self.encode_image(demo_img, activation=None) # b, dim_img_feat
         demo_vect = tf.tile(tf.expand_dims(demo_vect, axis=1), [1, self.max_len, 1]) # b, l, dim_img_feat
         dim_img_feat = demo_vect.get_shape().as_list()[-1]
-        posi_vect = tf.reshape(self.encode_image(posi_img), [-1, self.max_len, dim_img_feat]) # b, l, dim_img_feat
-        nega_vect = tf.reshape(self.encode_image(nega_img), [-1, self.max_len, dim_img_feat]) # b, l, dim_img_feat
+        posi_vect = tf.reshape(self.encode_image(posi_img, activation=None), [-1, self.max_len, dim_img_feat]) # b, l, dim_img_feat
+        nega_vect = tf.reshape(self.encode_image(nega_img, activation=None), [-1, self.max_len, dim_img_feat]) # b, l, dim_img_feat
 
         if self.dist == 'cos':
             # cos similarity
             posi_mask = tf.sequence_mask(posi_len, maxlen=self.max_len, dtype=tf.float32) # b, l
             nega_mask = tf.sequence_mask(nega_len, maxlen=self.max_len, dtype=tf.float32) # b, l
-            posi_sim = model_utils.cos_sim(demo_vect, posi_vect, axis=2)*posi_mask # b, l
-            nega_sim = model_utils.cos_sim(demo_vect, nega_vect, axis=2)*nega_mask # b, l
-            mean_posi_sim = tf.reduce_sum(posi_sim)/tf.reduce_sum(posi_mask)
-            mean_nega_sim = tf.reduce_sum(nega_sim)/tf.reduce_sum(nega_mask)
+
+            posi_sim = model_utils.cos_sim(demo_vect, posi_vect, axis=2)*posi_mask+(1-posi_mask) # b, l
+            nega_sim = model_utils.cos_sim(demo_vect, nega_vect, axis=2)*nega_mask-(1-nega_mask) # b, l
+
+            posi_sim = tf.sort(posi_sim, axis=1, direction='ASCENDING') # b, l
+            nega_sim = tf.sort(nega_sim, axis=1, direction='DESCENDING') # b, l
+
+            posi_min = tf.reduce_min(posi_sim, axis=1) # b
+            nega_max = tf.reduce_max(nega_sim, axis=1) # b
+            mean_extreme_dist = tf.reduce_mean(posi_min - nega_max)
+
+            mean_posi_sim = tf.reduce_sum(posi_sim*posi_mask)/tf.reduce_sum(posi_mask)
+            mean_nega_sim = tf.reduce_sum(nega_sim*nega_mask)/tf.reduce_sum(nega_mask)
+
             # loss
-            loss = mean_nega_sim + self.alpha - mean_posi_sim
+            loss = mean_nega_sim + self.alpha - mean_posi_sim - mean_extreme_dist
             # metric
             max_nega_sim = tf.tile(tf.reduce_max(nega_sim, axis=1, keepdims=True), [1, self.max_len]) # b, l
-            greater = tf.greater(posi_sim * posi_mask, max_nega_sim * posi_mask) # b, l
+            greater = tf.greater(posi_sim*posi_mask, max_nega_sim*posi_mask) # b, l
             metric = tf.reduce_sum(tf.cast(greater, dtype=tf.float32))/tf.reduce_sum(posi_mask)
 
             return metric, loss, posi_sim, nega_sim
@@ -124,12 +134,12 @@ class deep_metric(object):
             return metric, loss, posi_dist, nega_dist
 
 
-    def encode_image(self, inputs):
-        conv1 = model_utils.conv2d(inputs, 16, 3, 2, scope='conv1', max_pool=False)
-        conv2 = model_utils.conv2d(conv1, 32, 3, 2, scope='conv2', max_pool=False)
-        conv3 = model_utils.conv2d(conv2, 64, 3, 2, scope='conv3', max_pool=False)
-        conv4 = model_utils.conv2d(conv3, 128, 3, 2, scope='conv4', max_pool=False)
-        conv5 = model_utils.conv2d(conv4, 256, 3, 2, scope='conv5', max_pool=False)
+    def encode_image(self, inputs, activation=tf.nn.leaky_relu):
+        conv1 = model_utils.conv2d(inputs, 16, 3, 2, scope='conv1', max_pool=False, activation=activation)
+        conv2 = model_utils.conv2d(conv1, 32, 3, 2, scope='conv2', max_pool=False, activation=activation)
+        conv3 = model_utils.conv2d(conv2, 64, 3, 2, scope='conv3', max_pool=False, activation=activation)
+        conv4 = model_utils.conv2d(conv3, 128, 3, 2, scope='conv4', max_pool=False, activation=activation)
+        conv5 = model_utils.conv2d(conv4, 256, 3, 2, scope='conv5', max_pool=False, activation=activation)
         shape = conv5.get_shape().as_list()
         outputs = tf.reshape(conv5, shape=[-1, shape[1]*shape[2]*shape[3]]) # b*l, dim_img_feat
         return outputs
@@ -145,7 +155,7 @@ class deep_metric(object):
             })
 
     def valid(self, data):
-        demo_img, posi_img, nega_img, posi_len, nega_len= data
+        demo_img, posi_img, nega_img, posi_len, nega_len = data
         return self.sess.run([self.accuracy, self.loss, self.posi_dist, self.nega_dist], feed_dict={
             self.demo_img: demo_img,
             self.posi_img: posi_img,
